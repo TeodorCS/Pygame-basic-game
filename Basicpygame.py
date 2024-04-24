6f1e5150bf7701342d508b8de8ab904d3ba6b491
TeodorCS <82665680+TeodorCS@users.noreply.github.com>
2024-04-24 23:31:03 +0300

    Update Basicpygame.py

--- Basicpygame.py
+++ Basicpygame.py
@@ -2,34 +2,41 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+x=50
+y=50
+width = 20
+height = 30
+vel = 0.2
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300, 250, 20 ,20))
+
 
 run = True 
 
 while run:
 
-	screen.fill ((0, 0, 0))
-
-	pygame.draw.rect(screen, (0, 255 ,100), player)
-
-	key = pygame.key.get_pressed()
-	if key[pygame.K_a] == True:
-		player.move_ip(-1 , 0)
-	elif key[pygame.K_d] == True:
-		player.move_ip(+1 , 0)
-	elif key[pygame.K_w] == True:
-		player.move_ip(0 , -1)
-	elif key[pygame.K_s] == True:
-		player.move_ip(0 , +1)
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
+
+	key = pygame.key.get_pressed()
+	if key[pygame.K_a] and x > vel:
+		x -= vel
+	if key[pygame.K_d] and x < SCREEN_HEIGHT - width - vel:
+		x += vel
+	if key[pygame.K_w] and y > vel:
+		y -= vel
+	if key[pygame.K_s] and y < SCREEN_WIDTH - height - vel:
+		y += vel
+
+	screen.fill ((0, 0, 0))
+
+	pygame.draw.rect(screen, (0, 120 , 255), (x, y, width ,height))	
+
 	pygame.display.update()
 
 
