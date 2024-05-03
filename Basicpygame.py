ff28e11572d4a3bcbac78f239bef663847a956ee
TeodorCS <82665680+TeodorCS@users.noreply.github.com>
2024-05-03 22:59:58 +0300

    Update

--- Basicpygame.py
+++ Basicpygame.py
@@ -2,42 +2,108 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 564
+SCREEN_HEIGHT = 322
 
+clock = pygame.time.Clock()
+
+#character stats and positioning
 x=50
-y=50
-width = 20
-height = 30
-vel = 0.2
+y=220
+width = 64
+height = 64
+vel = 5
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Street Fight")
+
+walkRight = [pygame.image.load('Game\R1.png'), pygame.image.load('Game\R2.png'),pygame.image.load('Game\R3.png'), pygame.image.load('Game\R4.png'), pygame.image.load('Game\R5.png'), pygame.image.load('Game\R6.png'), pygame.image.load('Game\R7.png'), pygame.image.load('Game\R8.png'), pygame.image.load('Game\R9.png')]
+walkLeft = [pygame.image.load('Game\L1.png'), pygame.image.load('Game\L2.png'),pygame.image.load('Game\L3.png'), pygame.image.load('Game\L4.png'), pygame.image.load('Game\L5.png'), pygame.image.load('Game\L6.png'), pygame.image.load('Game\L7.png'), pygame.image.load('Game\L8.png'), pygame.image.load('Game\L9.png')]
+bg = pygame.image.load('Game\BGstreet.jpg')
+char = pygame.image.load('Game\standing.png')
+
+isJump = False
+jumpCount = 10
+left = False	
+right = False	
+walkCount = 0
+
+#drawing on the window
+
+def redrawGameWindow():
+	global walkCount
+
+	screen.blit(bg , (0,0))
+	
+	if walkCount + 1 >= 27:
+		walkCount = 0
+
+	if left:
+		screen.blit(walkLeft[walkCount//3], (x,y))
+		walkCount += 1
+
+	elif right:
+		screen.blit(walkRight[walkCount//3], (x,y))
+		walkCount += 1
+
+	else:
+		screen.blit(char, (x,y))
+
+	pygame.display.update()
 
 
+#main loop
 
 run = True 
-
 while run:
+
+	clock.tick(27)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
 
+	#movement features
+
 	key = pygame.key.get_pressed()
+
 	if key[pygame.K_a] and x > vel:
 		x -= vel
-	if key[pygame.K_d] and x < SCREEN_HEIGHT - width - vel:
+		left = True
+		right = False
+	elif key[pygame.K_d] and x < SCREEN_WIDTH - width - vel:
 		x += vel
-	if key[pygame.K_w] and y > vel:
-		y -= vel
-	if key[pygame.K_s] and y < SCREEN_WIDTH - height - vel:
-		y += vel
+		left = False
+		right = True
+	else: 
+		right = False
+		left = False
+		walkCount = 0
 
-	screen.fill ((0, 0, 0))
+	if not (isJump):
 
-	pygame.draw.rect(screen, (0, 120 , 255), (x, y, width ,height))	
+		if key[pygame.K_SPACE]:
+			isJump = True
+			right = False
+			left = False
+			walkCount = 0
+	else: 
 
-	pygame.display.update()
+		if jumpCount >= -10:
+			
+			neg = 1
+			
+			if jumpCount < 0:
+				neg = -1
 
+			y -= (jumpCount  ** 2) * 0.5 * neg 
+			jumpCount -= 1
+
+		else:
+			isJump = False
+			jumpCount = 10
+
+	
+	redrawGameWindow()
 
 pygame.quit()
